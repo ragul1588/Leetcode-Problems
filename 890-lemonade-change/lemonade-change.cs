aea9747b56a43631b9d8bedfe923808ade63d585
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2024-08-15 13:59:27 +0530

    Time: 339 ms (17.79%) | Memory: 57.6 MB (43.56%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cs
@@ -0,0 +1,44 @@
+public class Solution {
+    public bool LemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        foreach (int bill in bills)
+        {
+            if (bill == 5)
+            {
+                five++;
+            }
+            else if (bill == 10)
+            {
+                if (five > 0)
+                {
+                    five--;
+                    ten++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (bill == 20)
+            {
+                if (ten > 0 && five > 0)
+                {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3)
+                {
+                    five -= 3;
+                }
+                else
+                {
+                    return false; 
+                }
+            }
+        }
+
+        return true;
+    }
+}
