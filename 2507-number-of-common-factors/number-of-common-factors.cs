97b5b128a2ed4d5a9f0d303f81b1982c70cd0528
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2024-08-19 16:19:01 +0530

    Time: 18 ms (92.59%) | Memory: 26.6 MB (85.19%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.cs
+++ 2507-number-of-common-factors/number-of-common-factors.cs
@@ -1,16 +1,35 @@
 public class Solution {
-    public int CommonFactors(int a, int b) {
-           int min = Math.Min(a, b);
-        int count = 0;
-        
-        for (int i = 1; i <= min; i++)
+    public int CommonFactors(int a, int b)
+    {
+        int gcd = GCD(a, b);
+        return CountFactors(gcd);
+    }
+    
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
         {
-            if (a % i == 0 && b % i == 0)
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    private int CountFactors(int n)
+    {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
             {
-                count++;
+                count++; // i is a factor
+                if (i != n / i)
+                {
+                    count++; // (n / i) is also a factor
+                }
             }
         }
-        
         return count;
     }
 }
