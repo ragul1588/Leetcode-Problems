029a1c04bdbf755e378af1c0beb3695bfdf4d68e
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2024-09-22 20:34:22 +0530

    Time: 124 ms (16.30%) | Memory: 48.5 MB (93.39%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cs
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cs
@@ -1,18 +1,24 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
-       Dictionary<int,int> dict = new Dictionary<int,int>();
-
-       foreach(var dt in nums){
-         if(dict.ContainsKey(dt)){
-            dict[dt]++;
-         }
-         else{
-            dict[dt]=1;
-         }
-       }
-       foreach(int dt in nums){
-         if(dict[dt]==1) return dt;
-       }
-       return -1;
+           
+           int N = nums.Length;
+           if(N==1) return nums[0];
+           
+            for (int i = 0; i < N; i++)
+            {
+                if (nums[0] != nums[1]) return nums[0];
+                if (nums[N-1] != nums[N-2]) return nums[N-1];
+                     
+                else
+                {
+                    if(i==0) i++;
+                    if(i==N-1) i--;
+                    if (nums[i - 1] != nums[i] && nums[i] != nums[i + 1])
+                    {
+                        return nums[i];
+                    }
+                }
+            }
+            return -1;
     }
 }
