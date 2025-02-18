03bd350a8f05a3427cb6f871d7d4e5215078ba31
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-02-18 22:46:27 +0530

    Time: 70 ms (30.13%) | Memory: 39.8 MB (81.50%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/merge-strings-alternately.cs
@@ -1,26 +1,18 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
         
-        int w1 = word1.Length, w2 = word2.Length, i =0;
-        string result = "";
-
-        while(w1>0 && w2>0){
-            result +=word1[i];
-            result +=word2[i];
-            w1--;
-            w2--;
-            i++;
+        
+        while (i < word1.Length || j < word2.Length) {
+            if (i < word1.Length) {
+                result.Append(word1[i++]);
+            }
+            if (j < word2.Length) {
+                result.Append(word2[j++]);
+            }
         }
-        while(w1>0){
-            result +=word1[i];
-            w1--;
-            i++;
-        }
-        while(w2>0){
-            result +=word2[i];
-            w2--;
-            i++;
-        }
-      return result;
+        
+        return result.ToString();
     }
 }
