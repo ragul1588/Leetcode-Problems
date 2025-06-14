689f19540202eeb7c019aed12e697cf68e9c5825
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-06-14 19:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (39.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cs
+++ 42-trapping-rain-water/trapping-rain-water.cs
@@ -1,37 +1,29 @@
 public class Solution {
     public int Trap(int[] height) {
-        int[] leftArray = leftSide(height);
-        int[] rightArray = rightSide(height);
-         int n = height.Length;
-         int total = 0;
-
-        for(int i=0; i<n; i++){
-           int left = leftArray[i];
-           int right = rightArray[i];
-
-           if(left>height[i] && right>height[i]){
-              total += Math.Min(left,right) - height[i];
-           }
+        int n = height.Length;
+        int left = 0, leftMax = height[0], right = n-1, rightMax =0;
+        int total = 0;
+        while(left<right) {
+            if(height[left]<=height[right]){
+                if(height[left]<leftMax){
+                total += leftMax - height[left];
+            }
+            else{
+                leftMax = height[left];
+            }
+             left++;
+            }
+            else{
+                if(height[right]<rightMax){
+                    total += rightMax - height[right];
+                }
+                else {
+                    rightMax = height[right];
+                }
+                 right--;
+            }
+            
         }
         return total;
-    }
-
-    public int[] leftSide(int[] arr){
-      int n = arr.Length;
-      int[] left = new int[n];
-      left[0] = arr[0];
-      for(int i=1; i<n; i++){
-         left[i] = Math.Max(arr[i],left[i-1]);
-      }
-        return left;
-    }
-    public int[] rightSide(int[] arr){
-       int n = arr.Length;
-      int[] right = new int[n];
-      right[n-1] = arr[n-1];
-      for(int i=n-2; i>=0; i--){
-         right[i] = Math.Max(arr[i],right[i+1]);
-      }
-        return right;
     }
 }
