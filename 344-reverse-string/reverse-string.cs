019ab83c1f0f9690ee9e70bc1417acce284658c2
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-06-18 16:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 64.9 MB (66.80%) - LeetSync

--- 344-reverse-string/reverse-string.cs
+++ 344-reverse-string/reverse-string.cs
@@ -1,9 +1,14 @@
 public class Solution {
     public void ReverseString(char[] s) {
-       Stack<char> st = new Stack<char>();
-       foreach(char a in s) st.Push(a); 
-       for(int i=0; i<s.Length; i++){
-          s[i] = st.Pop();
-       } 
+       int left = 0;
+        int right = s.Length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
     }
 }
