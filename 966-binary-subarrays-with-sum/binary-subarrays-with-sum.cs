f8a199ff8b99750aa6737a04a224a7909579626c
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-07-20 11:25:38 +0530

    Time: 1 ms (100.00%) | Memory: 51 MB (81.65%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cs
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cs
@@ -3,16 +3,18 @@ public class Solution {
        return numOfSubarrayLessThanGoal(nums,goal) - numOfSubarrayLessThanGoal(nums,goal-1);
     }
     public int numOfSubarrayLessThanGoal(int[] nums, int goal){
-       int sum =0, right =0, left =0,count=0;
-        if(goal<0) return 0;
-       while(right<nums.Length){
-        sum +=nums[right];
+      
+      int sum = 0, left = 0,count= 0;
+      if(goal<0) return 0;
+      
+      for(int right=0; right<nums.Length; right++) {
+         sum +=nums[right];
+
           while(left<=right && sum>goal){
              sum -=nums[left];
              left++;
           }
           count += (right-left+1);
-          right++;
        }
        return count;
     }
