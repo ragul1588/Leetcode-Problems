724c5edb824463f8416ea9e2aa46fa9d740376f4
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-07-20 12:45:50 +0530

    Time: 83 ms (12.00%) | Memory: 56.7 MB (18.27%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        
+        if(s == null || s.Length == 0) return 0;
+        if(s == " ") return 1;
+        var li = new List<char>();
+        int n = s.Length,maxLen =0;
+        for(int i=0; i<n; i++){
+            li.Clear();
+             string result="";
+            for(int j=i; j<n; j++){
+              if(li.Contains(s[j])){
+                break;
+              }
+              else{
+                li.Add(s[j]);
+                result +=s[j];
+                maxLen = Math.Max(maxLen, result.Length);
+              }
+            }
+        }
+        return maxLen;
+    }
+}
