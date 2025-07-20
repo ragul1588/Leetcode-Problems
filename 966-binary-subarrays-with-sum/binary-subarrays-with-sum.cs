5a83b52f959b76da56d09be39e951d1eafd09a63
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-07-20 11:23:08 +0530

    Time: 2 ms (73.39%) | Memory: 50.8 MB (94.50%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cs
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cs
@@ -1,23 +1,19 @@
 public class Solution {
     public int NumSubarraysWithSum(int[] nums, int goal) {
-      var prefixCount = new Dictionary<int, int>();
-        prefixCount[0] = 1;  // to handle sum from index 0
-        int sum = 0, result = 0;
-
-        foreach (int num in nums) {
-            sum += num;
-
-            if (prefixCount.ContainsKey(sum - goal)) {
-                result += prefixCount[sum - goal];
-            }
-
-            if (!prefixCount.ContainsKey(sum)) {
-                prefixCount[sum] = 0;
-            }
-
-            prefixCount[sum]++;
-        }
-
-        return result;
+       return numOfSubarrayLessThanGoal(nums,goal) - numOfSubarrayLessThanGoal(nums,goal-1);
+    }
+    public int numOfSubarrayLessThanGoal(int[] nums, int goal){
+       int sum =0, right =0, left =0,count=0;
+        if(goal<0) return 0;
+       while(right<nums.Length){
+        sum +=nums[right];
+          while(left<=right && sum>goal){
+             sum -=nums[left];
+             left++;
+          }
+          count += (right-left+1);
+          right++;
+       }
+       return count;
     }
 }
