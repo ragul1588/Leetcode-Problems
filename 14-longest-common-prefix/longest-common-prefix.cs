dd0b71c320d893e04df4100454cc0094ff05875d
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-07-24 22:25:17 +0530

    Time: 2 ms (40.48%) | Memory: 43.2 MB (77.17%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+         if (strs == null || strs.Length == 0) return "";
+         string prefix = strs[0];
+
+         foreach(string s in strs){
+           while(!s.StartsWith(prefix)){
+              prefix = prefix.Substring(0,prefix.Length-1);
+           }
+           if(prefix.Length == 0) return ""; 
+         }
+         
+       return prefix;
+    }
+}
