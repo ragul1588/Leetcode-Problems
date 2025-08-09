64f0d5e39d3655b20fb50185f5740757ec8a8569
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-08-09 17:21:06 +0530

    Time: 2010 ms (25.30%) | Memory: 51.1 MB (77.11%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cs
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cs
@@ -1,35 +1,21 @@
 public class Solution {
     public int NumMatchingSubseq(string s, string[] words) {
-       
-     int No_OfSequence = 0;
-     
-      int n = s.Length, minLen = Int32.MaxValue, index = -1;
-        
+
+      int n = s.Length, minLen = Int32.MaxValue, index = -1,No_OfSequence = 0;
+      
         //Optimal solution
-     
-         //for(int i=0; i<n; i++){
-  
              foreach(string s2 in words){
                 int j=0;
                 int t=0;
                 int m = s2.Length;
                while(j<n && t<m){
                     if(s[j] == s2[t]) t++;
-                    
                     j++;
                 }
-                
                 if(t==m){
-                        No_OfSequence +=1;
+                    No_OfSequence +=1;
                 }
             }
-        //}
        return No_OfSequence;
     }
-        
-        //return index == -1 ? "" : s1.Substring(index,minLen);
-       
-    
-
-     
 }
