5bae26d51e6e81257821d762eea4b431d794a571
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-09-14 15:35:36 +0530

    Time: 2 ms (99.43%) | Memory: 62 MB (11.90%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cs
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cs
@@ -1,19 +1,17 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k) {
          int left = 0, maxLen = 0,n=nums.Length,zeros=0;
-      
+        
+        //Optimal Solution
+
         for(int right=0; right<n; right++){
           if(nums[right]==0){
             zeros++;
           }
-
           if(zeros>k){
-            if(nums[left]==0){
-                zeros--;
-            }
+            if(nums[left]==0) zeros--;
             left++;
           }
-
           maxLen = Math.Max(maxLen, right-left+1);
         }
         return maxLen;
